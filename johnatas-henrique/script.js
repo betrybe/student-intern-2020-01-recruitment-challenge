e3a115b6414c604ea8791cf75c82431600a1ef03
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-21 03:35:56 -0300

    Projeto finalizado!

--- johnatas-henrique/script.js
+++ johnatas-henrique/script.js
@@ -1,8 +1,19 @@
 const imgTakeThis = document.querySelector('#take-this');
 const divTakeThis = document.querySelector('#box-take-this');
+const caixaGabarito = document.querySelector('#caixa-gabarito');
+const divGabarito = document.querySelector('#div-gabarito');
+const navLinkGabarito = document.querySelector('#nav-gabarito');
 
 function abreCaixa () {
     divTakeThis.classList.remove('esconder');
 }
 
+function mostraGabarito () {
+    if(caixaGabarito.value === 'RESPOSTAS'){
+        divGabarito.classList.remove('esconder');
+        navLinkGabarito.classList.remove('esconder');
+    }
+}
+
+caixaGabarito.addEventListener('keyup', mostraGabarito);
 imgTakeThis.addEventListener('click', abreCaixa);
